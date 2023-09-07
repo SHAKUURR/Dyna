cf520dec37e18346cdb2b791a170a9b3aea9cda7
iamshakur <salmanshakur7@gmail.com>
2023-09-07 23:48:03 +0100

    completed, waiting for review

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+const header = document.querySelector(".nav-section");
+const elements = document.querySelector(".nav-list");
+const menu = document.querySelectorAll(".nav-link");
+const icon = document.querySelector("#menu-icon i");
+
+menu.forEach((element) => {
+	element.addEventListener("click", () => {
+		elements.classList.toggle("active");
+		icon.classList.toggle("active");
+	});
+});
+
+//Animation on scroll........................
+ScrollReveal({ distance: "60px", duration: 2000, delay: 400 });
+
+ScrollReveal().reveal(".hero, .about, .automate, .trade, .provide", {
+	delay: 110,
+	origin: "bottom",
+	interval: 500,
+	duration: 2200,
+});
